a506a69073533310e7ba26fe6cd82ee8d4be37ca
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2022-10-30 16:47:52 +0100

    fix: list comprehension in config.py

--- src/kermapy/config.py
+++ src/kermapy/config.py
@@ -1,19 +1,14 @@
 import os
 
 
-def __get_bootstrap_nodes() -> dict[str, str]:
+def _get_bootstrap_nodes() -> dict[str, str]:
     raw_nodes = os.environ.get("BOOTSTRAP_NODES", "128.130.122.101:18018")
     nodes = raw_nodes.split(",")
-    dict = {}
-
-    for item in nodes:
-        dict[item] = ""
-
-    return dict
+    return {item: "" for item in nodes}
 
 
 VERSION = "1.0.2"
-BOOTSTRAP_NODES = __get_bootstrap_nodes()
+BOOTSTRAP_NODES = _get_bootstrap_nodes()
 LISTEN_ADDR = os.environ.get("LISTEN_ADDR", "0.0.0.0:18018")
 STORAGE_PATH = os.environ.get("STORAGE_PATH", "../../data/storage.json")
 CLIENT_CONNECTIONS = int(os.environ.get("CLIENT_CONNECTIONS", 8))
