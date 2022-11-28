93c98799aea9c490a3ad30ec4d2080d677eae8fd
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2022-11-28 13:33:20 +0100

    refactor: inline height handling and use self.height inside objects.py

--- src/kermapy/objects.py
+++ src/kermapy/objects.py
@@ -8,14 +8,6 @@ import plyvel
 from org.webpki.json.Canonicalize import canonicalize
 
 
-def to_u256(b: bytes) -> int:
-    return int.from_bytes(b, 'big', signed=False)
-
-
-def from_u256(i: int) -> bytes:
-    return int.to_bytes(i, 256, 'big', signed=False)
-
-
 class Objects:
     def __init__(self, storage_path: str):
         self._db: plyvel.DB = plyvel.DB(storage_path, create_if_missing=True)
@@ -30,7 +22,10 @@ class Objects:
         return hashlib.sha256(canonical_object).hexdigest()
 
     def height(self, object_id: str) -> int:
-        return to_u256(self._db.get(b'height:' + bytes.fromhex(object_id)))
+        value = self._db.get(b'object:' + bytes.fromhex(object_id))
+        if not value:
+            raise KeyError(object_id)
+        return int.from_bytes(value, 'big', signed=False)
 
     def get(self, object_id: str) -> dict:
         value = self._db.get(b'object:' + bytes.fromhex(object_id))
@@ -46,11 +41,10 @@ class Objects:
 
         if obj["type"] == "block":
             if obj["previd"]:
-                height = to_u256(self._db.get(b'height:' + bytes.fromhex(obj["previd"])))
-                height += 1
+                height = self.height(obj["previd"]) + 1
             else:
                 height = 0
-            self._db.put(b'height:' + bytes.fromhex(object_id), from_u256(height))
+            self._db.put(b'height:' + bytes.fromhex(object_id), int.to_bytes(height, 256, 'big', signed=False))
         self._db.put(b'object:' + bytes.fromhex(object_id), canonicalize(obj))
 
     def event_for(self, object_id: str) -> asyncio.Event:
