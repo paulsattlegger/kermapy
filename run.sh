f20d258dea40c51331f4a98c7610cbc0e197e67d
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2022-12-19 14:46:51 +0100

    fix: disable connection to peers by default for run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ TAG='kermapy:latest'
 
 docker build --tag="$TAG" .
 docker run --interactive --rm --tty \
---env 'STORAGE_PATH=/app/data' \
+--env='STORAGE_PATH=/app/data' \
+--env='CLIENT_CONNECTIONS=0' \
 --publish='127.0.0.1:18018:18018' \
 --volume="$(pwd)/data:/app/data" \
 "$TAG"
