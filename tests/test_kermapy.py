97287f6c12ae33850f09cf9afff3d8de4b35889a
Marcel Juen <marcel.juen@raincoat.systems>
2023-01-23 00:10:04 +0100

    Added additional read_dict to read ihaveobject message in test

--- tests/test_kermapy.py
+++ tests/test_kermapy.py
@@ -1734,6 +1734,7 @@ class Task4TestCase(KermaTestCase):
 
         await client1.close()
 
+
 class Task5TestCase(KermaTestCase):
     async def append_block1(self, client):
         cb_block1_after_genesis = {
@@ -1769,41 +1770,41 @@ class Task5TestCase(KermaTestCase):
 
     async def append_block2(self, client):
         cb_block2_after_genesis = {
-                "height": 2, "outputs": [
-                    {
-                        "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
-                        "value": 50000000000000
-                    }],
-                "type": "transaction"
-                }
+            "height": 2, "outputs": [
+                {
+                    "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
+                    "value": 50000000000000
+                }],
+            "type": "transaction"
+        }
         ihaveobject_message = {
-                "type": "ihaveobject",
-                "objectid": "73231cc901774ddb4196ee7e9e6b857b208eea04aee26ced038ac465e1e706d2"
-            }
+            "type": "ihaveobject",
+            "objectid": "73231cc901774ddb4196ee7e9e6b857b208eea04aee26ced038ac465e1e706d2"
+        }
         self.assertDictEqual(ihaveobject_message, await client.write_tx(cb_block2_after_genesis))
 
         tx_block2_after_genesis = {
-                "inputs": [
-                    {
-                        "outpoint": {
-                            "index": 0,
-                            "txid": "2a9458a2e75ed8bd0341b3cb2ab21015bbc13f21ea06229340a7b2b75720c4df"
-                        },
-                        "sig": "49cc4f9a1fb9d600a7debc99150e7909274c8c74edd7ca183626dfe49eb4aa21c6ff0e4c5f0dc2a328ad6b8ba10bf7169d5f42993a94bf67e13afa943b749c0b"
-                    }
-                ],
-                "outputs": [
-                    {
-                        "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
-                        "value": 50
-                    }
-                ],
-                "type": "transaction"
-            }
+            "inputs": [
+                {
+                    "outpoint": {
+                        "index": 0,
+                        "txid": "2a9458a2e75ed8bd0341b3cb2ab21015bbc13f21ea06229340a7b2b75720c4df"
+                    },
+                    "sig": "49cc4f9a1fb9d600a7debc99150e7909274c8c74edd7ca183626dfe49eb4aa21c6ff0e4c5f0dc2a328ad6b8ba10bf7169d5f42993a94bf67e13afa943b749c0b"
+                }
+            ],
+            "outputs": [
+                {
+                    "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
+                    "value": 50
+                }
+            ],
+            "type": "transaction"
+        }
         ihaveobject_message = {
-                "type": "ihaveobject",
-                "objectid": "7ef80f2da40b3f681a5aeb7962731beddccea25fa51e6e7ae6fbce8a58dbe799"
-            }
+            "type": "ihaveobject",
+            "objectid": "7ef80f2da40b3f681a5aeb7962731beddccea25fa51e6e7ae6fbce8a58dbe799"
+        }
         self.assertDictEqual(ihaveobject_message, await client.write_tx(tx_block2_after_genesis))
 
         block_message = {
@@ -1822,7 +1823,7 @@ class Task5TestCase(KermaTestCase):
             },
             "type": "object"
         }
-            
+
         ihaveobject_message = {
             "type": "ihaveobject",
             "objectid": "000000021dc4cfdcd0970084949f94da17f97504e1cc3e354851bb4768842b57"
@@ -1830,25 +1831,24 @@ class Task5TestCase(KermaTestCase):
         await client.write_dict(block_message)
         self.assertDictEqual(ihaveobject_message, await client.read_dict())
 
-
-    # Grader 1 sends one of the following invalid objects in an object message. 
-    # Grader 1 must receive an error message, 
+    # Grader 1 sends one of the following invalid objects in an object message.
+    # Grader 1 must receive an error message,
     # and Grader 2 must not receive an ihaveobject message with the corresponding object id.
     # a) A transaction with two inputs that share an outpoint.
 
     async def test_sendInvalidObjectWithTwoInputAndOneOutput(self):
         private_key = ed25519.Ed25519PrivateKey.generate()
         pubkey_hex = private_key.public_key().public_bytes(_serialization.Encoding.Raw, _serialization.PublicFormat.Raw).hex()
-        
+
         client1 = await Client.new_established()
 
         initial_transaction = {
-            "height":1,
+            "height": 1,
             "outputs": [{
                 "pubkey": None,
-                "value":50000000000000}
+                "value": 50000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         initial_transaction["outputs"][0]["pubkey"] = pubkey_hex
         txid = await client1.write_tx(initial_transaction)
@@ -1856,16 +1856,16 @@ class Task5TestCase(KermaTestCase):
 
         client2 = await Client.new_established()
 
-        invalid_transaction_with_two_inputs =  {
+        invalid_transaction_with_two_inputs = {
             "inputs": [{
-                "outpoint": { 
-                    "index":0,
+                "outpoint": {
+                    "index": 0,
                     "txid": txid
                 },
                 "sig": None
             },
-            {
-                "outpoint": { 
+                {
+                "outpoint": {
                     "index": 0,
                     "txid": txid
                 },
@@ -1874,15 +1874,15 @@ class Task5TestCase(KermaTestCase):
             ],
             "outputs": [{
                 "pubkey": "4a7f4fb59ee4b3b2f940cf0efb6a09d9b74e8f30f9f8cc381e77fe8f69e996e2",
-                "value":49000000000000}
+                "value": 49000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         sig = private_key.sign(bytes.fromhex(canonicalize(copy.deepcopy(invalid_transaction_with_two_inputs)).hex())).hex()
 
         invalid_transaction_with_two_inputs['inputs'][0]['sig'] = sig
         invalid_transaction_with_two_inputs['inputs'][1]['sig'] = sig
-        
+
         response = await client1.write_tx(invalid_transaction_with_two_inputs)
 
         self.assertIn("error", response['type'])
@@ -1892,7 +1892,7 @@ class Task5TestCase(KermaTestCase):
 
         await client1.close()
         await client2.close()
-    
+
     # b) A block with more than 128 characters in the note field.
     async def test_sendInvalidBlockWithNoteOver128(self):
 
@@ -1915,7 +1915,6 @@ class Task5TestCase(KermaTestCase):
             },
             "type": "object"
         }
-      
 
         await client1.write_dict(invalid_block_with_too_long_note)
 
@@ -1927,30 +1926,30 @@ class Task5TestCase(KermaTestCase):
 
         await client1.close()
         await client2.close()
-   
-    # Grader 1 sends a valid transaction with two inputs (spending outputs with different public keys). 
+
+    # Grader 1 sends a valid transaction with two inputs (spending outputs with different public keys).
     # Grader 2 must receive the transaction when it sends a getobject with the corresponding transaction id.
     async def test_sendValidTransactionAndReceiveItWithGetObject(self):
         private_key = ed25519.Ed25519PrivateKey.generate()
         pubkey_hex = private_key.public_key().public_bytes(_serialization.Encoding.Raw, _serialization.PublicFormat.Raw).hex()
-        
+
         client1 = await Client.new_established()
 
         first_transaction = {
-            "height":1,
+            "height": 1,
             "outputs": [{
                 "pubkey": None,
-                "value":50000000000000}
+                "value": 50000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         second_transaction = {
-            "height":1,
+            "height": 1,
             "outputs": [{
                 "pubkey": None,
-                "value":5000000000000}
+                "value": 5000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         first_transaction["outputs"][0]["pubkey"] = pubkey_hex
         txid1 = await client1.write_tx(first_transaction)
@@ -1961,16 +1960,16 @@ class Task5TestCase(KermaTestCase):
 
         client2 = await Client.new_established()
 
-        transaction_with_two_inputs =  {
+        transaction_with_two_inputs = {
             "inputs": [{
-                "outpoint": { 
-                    "index":0,
+                "outpoint": {
+                    "index": 0,
                     "txid": txid1
                 },
                 "sig": None
             },
-            {
-                "outpoint": { 
+                {
+                "outpoint": {
                     "index": 0,
                     "txid": txid2
                 },
@@ -1979,15 +1978,15 @@ class Task5TestCase(KermaTestCase):
             ],
             "outputs": [{
                 "pubkey": "4a7f4fb59ee4b3b2f940cf0efb6a09d9b74e8f30f9f8cc381e77fe8f69e996e2",
-                "value":49000000000000}
+                "value": 49000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         sig = private_key.sign(bytes.fromhex(canonicalize(copy.deepcopy(transaction_with_two_inputs)).hex())).hex()
 
         transaction_with_two_inputs['inputs'][0]['sig'] = sig
         transaction_with_two_inputs['inputs'][1]['sig'] = sig
-        
+
         response = await client1.write_tx(transaction_with_two_inputs)
 
         response = await client2.read_dict()
@@ -2002,7 +2001,7 @@ class Task5TestCase(KermaTestCase):
     async def test_sendValidMempoolAndChain(self):
         client1 = await Client.new_established()
         await self.append_block1(client1)
-        
+
         mempool_transaction = {
             "inputs": [
                 {
@@ -2020,8 +2019,8 @@ class Task5TestCase(KermaTestCase):
                 }
             ],
             "type": "transaction"
-        } 
-        
+        }
+
         txid = await client1.write_tx(mempool_transaction)
         txid = txid['objectid']
 
@@ -2033,13 +2032,13 @@ class Task5TestCase(KermaTestCase):
 
         self.assertIn(txid, mempool_response['txids'][0])
 
-    # b) Grader 1 sends a transaction that is valid with respect to the mempool state. 
-    # Grader 1 again sends a getmempool message, 
+    # b) Grader 1 sends a transaction that is valid with respect to the mempool state.
+    # Grader 1 again sends a getmempool message,
     # and this time the mempool should contain the sent transaction.
     async def test_mempoolContainsSentTransaction(self):
         private_key = ed25519.Ed25519PrivateKey.generate()
         pubkey_hex = private_key.public_key().public_bytes(_serialization.Encoding.Raw, _serialization.PublicFormat.Raw).hex()
-        
+
         client1 = await Client.new_established()
         await self.append_block1(client1)
 
@@ -2061,7 +2060,7 @@ class Task5TestCase(KermaTestCase):
             ],
             "type": "transaction"
         }
-        
+
         txid = (await client1.write_tx(mempool_transaction))
 
         await client1.write(GET_MEMPOOL)
@@ -2069,16 +2068,16 @@ class Task5TestCase(KermaTestCase):
 
         self.assertIn(txid['objectid'], mempool_response['txids'][0])
 
-    # c) Grader 1 sends a transaction that is invalid with respect to the mempool state. 
-    # Grader 1 again sends a getmempool message, 
+    # c) Grader 1 sends a transaction that is invalid with respect to the mempool state.
+    # Grader 1 again sends a getmempool message,
     # and this time the mempool should not contain the sent transaction.
     async def test_mempoolContainsNotInvalidSentTransaction(self):
         client1 = await Client.new_established()
         await self.append_block1(client1)
         invalid_transaction = {
             "inputs": [{
-                "outpoint": { 
-                    "index":0,
+                "outpoint": {
+                    "index": 0,
                     "txid": 'test'
                 },
                 "sig": 'test'
@@ -2086,9 +2085,9 @@ class Task5TestCase(KermaTestCase):
             ],
             "outputs": [{
                 "pubkey": "4a7f4fb59ee4b3b2f940cf0efb6a09d9b74e8f30f9f8cc381e77fe8f69e996e2",
-                "value":49000000000000}
+                "value": 49000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         response = await client1.write_tx(invalid_transaction)
 
@@ -2100,18 +2099,18 @@ class Task5TestCase(KermaTestCase):
 
         self.assertEqual(0, len(mempool_response['txids']))
 
-    # d) Grader 1 sends a coinbase transaction. 
+    # d) Grader 1 sends a coinbase transaction.
     # Grader 1 again sends a getmempool messageand this time the mempool should not contain the sent transaction.
     async def test_mempoolContainsNotCainbaseSentTransaction(self):
         client1 = await Client.new_established()
         await self.append_block1(client1)
         invalid_transaction = {
-            "height":1,
+            "height": 1,
             "outputs": [{
                 "pubkey": "Test",
-                "value":50000000000000}
+                "value": 50000000000000}
             ],
-            "type":"transaction"
+            "type": "transaction"
         }
         response = await client1.write_tx(invalid_transaction)
 
@@ -2123,44 +2122,44 @@ class Task5TestCase(KermaTestCase):
 
         self.assertEqual(0, len(mempool_response['txids']))
 
-    # e) Grader 1 will send a longer chain (causing a reorg) and then send a getmempool message. 
-    # The received mempool must be consistent with the new chain: 
-    # i. It must not contain any transactions that are already in the new chain or 
+    # e) Grader 1 will send a longer chain (causing a reorg) and then send a getmempool message.
+    # The received mempool must be consistent with the new chain:
+    # i. It must not contain any transactions that are already in the new chain or
     # are invalid with respect to the new chain UTXO state.
     async def test_mempoolContainsNoTransactionFromLongerChain(self):
         client1 = await Client.new_established()
         await self.append_block1(client1)
-        
+
         cb_block2_after_genesis = {
-                "height": 2, "outputs": [
-                    {
-                        "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
-                        "value": 50000000000000
-                    }],
-                "type": "transaction"
-                }
+            "height": 2, "outputs": [
+                {
+                    "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
+                    "value": 50000000000000
+                }],
+            "type": "transaction"
+        }
         await client1.write_tx(cb_block2_after_genesis)
 
         tx_block2_after_genesis = {
-                "inputs": [
-                    {
-                        "outpoint": {
-                            "index": 0,
-                            "txid": "2a9458a2e75ed8bd0341b3cb2ab21015bbc13f21ea06229340a7b2b75720c4df"
-                        },
-                        "sig": "49cc4f9a1fb9d600a7debc99150e7909274c8c74edd7ca183626dfe49eb4aa21c6ff0e4c5f0dc2a328ad6b8ba10bf7169d5f42993a94bf67e13afa943b749c0b"
-                    }
-                ],
-                "outputs": [
-                    {
-                        "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
-                        "value": 50
-                    }
-                ],
-                "type": "transaction"
-            }
+            "inputs": [
+                {
+                    "outpoint": {
+                        "index": 0,
+                        "txid": "2a9458a2e75ed8bd0341b3cb2ab21015bbc13f21ea06229340a7b2b75720c4df"
+                    },
+                    "sig": "49cc4f9a1fb9d600a7debc99150e7909274c8c74edd7ca183626dfe49eb4aa21c6ff0e4c5f0dc2a328ad6b8ba10bf7169d5f42993a94bf67e13afa943b749c0b"
+                }
+            ],
+            "outputs": [
+                {
+                    "pubkey": "c7c2c13afd02be7986dee0f4630df01abdbc950ea379055f1a423a6090f1b2b3",
+                    "value": 50
+                }
+            ],
+            "type": "transaction"
+        }
         objectid = (await client1.write_tx(tx_block2_after_genesis))["objectid"]
-        
+
         await client1.write(GET_CHAINTIP)
         chaintip_response = await client1.read_dict()
         self.assertIn("0000000108bdb42de5993bcf5f7d92557585dd6abfe9fb68e796518fe7f2ed2e", chaintip_response['blockid'])
@@ -2195,11 +2194,11 @@ class Task5TestCase(KermaTestCase):
 
         await client1.write(GET_MEMPOOL)
         mempool_response = await client1.read_dict()
-        self.assertEqual(0,len(mempool_response['txids']))
-        
+        self.assertEqual(0, len(mempool_response['txids']))
 
-    # ii. It must also contain transactions that were in the old chain but 
+    # ii. It must also contain transactions that were in the old chain but
     # are not in the new chain and valid with respect to the new chain UTXO state.
+
     async def test_mempoolContainsTransactionFromOldChain(self):
         client1 = await Client.new_established()
         client2 = await Client.new_established()
@@ -2212,7 +2211,7 @@ class Task5TestCase(KermaTestCase):
 
         await client1.write(GET_MEMPOOL)
         mempool_response1 = await client1.read_dict()
-        self.assertEqual(0,len(mempool_response1['txids']))
+        self.assertEqual(0, len(mempool_response1['txids']))
 
         second_chain_first_block_message = {
             "object": {
@@ -2240,21 +2239,23 @@ class Task5TestCase(KermaTestCase):
             },
             "type": "object"
         }
-       
+
         await client1.write_dict(second_chain_second_block_message)
         await client1.readline()
 
         second_chain_third_block_message = {
-            "T":"00000002af000000000000000000000000000000000000000000000000000000",
-            "created": 1674424538, "miner":"Kermars",
-            "nonce":"000000000000000000000000000000000000000000000000e000000014949e54",
-            "note":"Third block of second chain",
-            "previd":"000000024297ac6fe162c32dd1f43d2352adec27c1f36ccdd6e7cf0c8b5ed40b",
-            "txids":[],
-            "type":"block"
+            "T": "00000002af000000000000000000000000000000000000000000000000000000",
+            "created": 1674424538, "miner": "Kermars",
+            "nonce": "000000000000000000000000000000000000000000000000e000000014949e54",
+            "note": "Third block of second chain",
+            "previd": "000000024297ac6fe162c32dd1f43d2352adec27c1f36ccdd6e7cf0c8b5ed40b",
+            "txids": [],
+            "type": "block"
         }
-        
+
         await client2.write_tx(second_chain_third_block_message)
+        # Read ihaveobject
+        await client1.read_dict()
 
         await client1.write(GET_CHAINTIP)
         chaintip_response = await client1.read_dict()
@@ -2263,7 +2264,6 @@ class Task5TestCase(KermaTestCase):
         await client1.write(GET_MEMPOOL)
         mempool_response = await client1.read_dict()
         self.assertIn("7ef80f2da40b3f681a5aeb7962731beddccea25fa51e6e7ae6fbce8a58dbe799", mempool_response['txids'][0])
-
 
 
 if __name__ == "__main__":
